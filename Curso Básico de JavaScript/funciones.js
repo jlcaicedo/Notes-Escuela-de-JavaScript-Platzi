c33dd35a68b0394f4e3ea8c3afaf0ff21ad8c2bd
Jose Caicedo <jlcaicedo@outlook.com>
2021-01-11 23:53:26 -0500

    Clase Variables y Funciones

---
+++ Curso Básico de JavaScript/funciones.js
@@ -0,0 +1,12 @@
+// dos tipos de funciones
+//1. Funciones Declarativas
+function miFuncion() {
+    return 3;
+}
+miFuncion();
+//2. Funciones Expresivas
+var miFuncion = function(){
+    return a + b;
+}
+
+miFuncion();
---
+++ Curso Básico de JavaScript/variables.js
@@ -0,0 +1,17 @@
+var nombre = "Jose";
+
+    // declarar
+    var edad;
+    // inicializar
+    edad = 30;
+
+    edad
+
+    // Tipo String 
+    var elementos = ["computadoras","celular"];
+
+    //tipo Objeto
+    var persona = {
+        nombre: "diego",
+        edad: 30,
+    }
