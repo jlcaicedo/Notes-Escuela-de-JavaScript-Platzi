d2958882242b3af15c9d983b3583bea649471672
Jose Caicedo <jlcaicedo@outlook.com>
2021-01-15 10:04:18 -0500

    Variables: Numeros

--- Fundamentos de JavaScript/variables.js
+++ Fundamentos de JavaScript/variables.js
@@ -17,3 +17,25 @@ var cantidadDeLetrasDelNombre = nombre.length
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}` //Interpolacion de texto se usa con comilla invertida
 
 var str = nombre.substr(1, 2)
+
+//laas variable nuwemericas se pueden expresar de diferentes maneras
+// Manera 1: incremento de la edad en 1
+edad = edad + 1
+
+// Manera 2: igualmente incrementa den uno una segunda manera
+edad += 1
+
+// Igualmente se puede usar para otro tipos de operaciones
+peso -= 2
+
+var precioDeVino = 25.09
+
+var cantidadCompra = 3
+
+var total = precioDeVino * 100 * cantidadCompra / 100
+
+// Tambien se puede usar la forma de redondear 
+var total =Math.round ( precioDeVino * 100 * cantidadCompra ) / 100
+var totalStr = total.toFixed(3)
+// toFixed nos convierte a string, pero esto lo podemos volver a convertir con parseFloat
+var total2 = parseFloat(totalStr)
