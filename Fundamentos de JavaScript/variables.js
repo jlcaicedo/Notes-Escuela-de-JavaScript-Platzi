5524d7cce07cda08158630537d410e64dec90592
Jose Caicedo <jlcaicedo@outlook.com>
2021-01-15 09:48:50 -0500

    Variables: Strings

--- Fundamentos de JavaScript/variables.js
+++ Fundamentos de JavaScript/variables.js
@@ -7,3 +7,13 @@ console.log('Hola ' + nombre + ' ' + apellido)
 console.log('Tengo ' + edad)
 
 var peso = 75
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}` //Interpolacion de texto se usa con comilla invertida
+
+var str = nombre.substr(1, 2)
