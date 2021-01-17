ddb9ecded62b0a076b961937ac2aad22d5140907
Jose Caicedo <jlcaicedo@outlook.com>
2021-01-16 21:17:44 -0500

    Condicionales

--- Fundamentos de JavaScript/condicionales.js
+++ Fundamentos de JavaScript/condicionales.js
@@ -42,17 +42,17 @@ function imprimirProfesiones(persona) {
 }
 imprimirProfesiones(jose)
 
-imprimirProfesiones({
-    nombre: 'Stephanie',
-    apellido: 'Rojas',
-    edad: '29',
+var coco = {
+    nombre: 'Coco',
+    apellido: 'Caicedo',
+    edad: '6',
     ingeniero: false,
     cocinero: true,
     dj: false,
     cantante: true,
     guitarrista: false,
     runner: false,
-})
+}
 
 // Ejercicio 
 
@@ -77,3 +77,20 @@ function imprimirSiEsMayorDeEdad(persona) {
     runner: false,
 })
 
+// Funciones que retornan valores
+
+//Magin Number
+const MAYORIA_DE_EDAD = 18 // Definimos en el scoop global esata variable, como no se puede modificar se pone como constante = const y se escriben en mayuscula separado por guines bajo.
+
+
+function esMayorDeEdad(persona) {
+    return persona.edad >= MAYORIA_DE_EDAD
+} // creamos esta funcion para que nos devuelva si es true o false para tener mejor legibilidad del codigo
+
+function imprimirSiEsMayorDeEdad(persona) {
+    if (esMayorDeEdad(persona)) {
+         console.log(`${persona.nombre} es mayor de edad, tiene ${persona.edad} annos`)
+     }  else {
+        console.log(`${persona.nombre} es menor de edad, tiene ${persona.edad} annos`)
+     }
+}
