61766b7d423f8a1386fc5573b755e81ffb10007b
Jose Caicedo <jlcaicedo@outlook.com>
2021-01-19 23:06:02 -0500

    Clase Estructuras repetitivas: for

---
+++ Fundamentos de JavaScript/ciclos.js
@@ -0,0 +1,26 @@
+var jose = {
+    nombre: 'Jose',
+    apellido: 'Caicedo',
+    edad: 37,
+    peso: 75,
+}
+
+console.log(`Al inicio del anno ${jose.nombre} pesa ${jose.peso}Kg`)
+
+const aumentarPeso = persoona => persoona.peso += 200
+
+//el ciclo for tiene tres parte iniciales:
+// (parte inicial; parte de la condicion ; parter del incremento )
+// (el contador debe tener un nombre [var i] e indicarle en que valor inicia [= 1]; la condicion es que sea menoir a 365 [i <= 365]; ahora debemos indicarle el incremento con [i++]) 
+for (var i = 1; i <= 365; i++) {
+    var random = Math.random()
+
+    if (random < 0.25) {
+
+    } else if (random < 0.5) {
+
+    }
+    
+}
+
+console.log(`Al final del anno ${jose.nombre} pesa ${jose.peso}Kg`)
